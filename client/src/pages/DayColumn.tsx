f384decbfe501f34f4bc6ce4896e37bf84656a96
TeeGree <tsgriffi@gmail.com>
2022-10-01 14:13:48 -0400

    Refactor api calls to reduce unnecessary fetches.

--- client/src/pages/DayColumn.tsx
+++ client/src/pages/DayColumn.tsx
@@ -1,157 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import classes from './DayColumn.module.scss';
-import path from 'path-browserify';
 import { Day } from '../types/Day';
-import { DropdownOption } from '../types/DropdownOption';
 import { Button } from '@mui/material';
 import { Add } from '@mui/icons-material';
 import { EditableTimeSlip } from './EditableTimeSlip';
-
-const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
-
-interface TaskMap {
-    projectId: number;
-    allowedTaskIds: number[];
-}
+import { DropdownOption } from '../types/DropdownOption';
 
 interface DayColumnProps {
     date: Date;
     day: Day;
     isCurrentDay: boolean;
+    projectOptions: DropdownOption[];
+    laborCodeOptions: DropdownOption[];
+    getTaskOptionsForProject: (projectId: number) => DropdownOption[];
+    saveTimeSlip: (
+        projectId: number,
+        taskId: number | null,
+        laborCodeId: number | null,
+        hours: number,
+        minutes: number,
+        date: Date,
+    ) => Promise<Response>;
 }
 
 export const DayColumn: React.FC<DayColumnProps> = (props: DayColumnProps) => {
-    const [projects, setProjects] = useState<DropdownOption[]>([]);
-    const [tasks, setTasks] = useState<Map<number, string>>(new Map<number, string>([]));
-
-    const [tasksAllowedForProjects, setTasksAllowedForProjects] = useState<Map<number, number[]>>(
-        new Map<number, number[]>([]),
-    );
-
-    const [laborCodes, setLaborCodes] = useState<DropdownOption[]>([]);
-
     const [addingTimeSlip, setAddingTimeSlip] = useState(false);
 
-    const { date, day, isCurrentDay } = props;
+    const {
+        date,
+        day,
+        isCurrentDay,
+        projectOptions,
+        laborCodeOptions,
+        getTaskOptionsForProject,
+        saveTimeSlip,
+    } = props;
 
-    const getProjects = async () => {
-        if (apiEndpoint === undefined) {
-            throw Error('No REACT_APP_API_ENDPOINT has been set!');
-        }
-        const url = path.join(apiEndpoint, 'Project');
-        const result = await fetch(url, { credentials: 'include' });
-
-        const projectsFromApi: DropdownOption[] = await result.json();
-        setProjects(projectsFromApi);
+    const getDateString = () => {
+        return date.toLocaleDateString('en');
     };
 
-    const getTasks = async () => {
-        if (apiEndpoint === undefined) {
-            throw Error('No REACT_APP_API_ENDPOINT has been set!');
-        }
-        const url = path.join(apiEndpoint, 'Task');
-        const result = await fetch(url, { credentials: 'include' });
-
-        const tasksFromApi: DropdownOption[] = await result.json();
-
-        const taskMap = new Map<number, string>([]);
-        tasksFromApi.forEach((task) => taskMap.set(task.id, task.name));
-        setTasks(taskMap);
-    };
-
-    const getTasksAllowedForProjects = async () => {
-        if (apiEndpoint === undefined) {
-            throw Error('No REACT_APP_API_ENDPOINT has been set!');
-        }
-        const url = path.join(apiEndpoint, 'Task/ProjectsAllowedTasks');
-        const result = await fetch(url, { credentials: 'include' });
-
-        const taskMapsFromApi: TaskMap[] = await result.json();
-
-        const taskProjectMap = new Map<number, number[]>([]);
-        taskMapsFromApi.forEach((taskMap) =>
-            taskProjectMap.set(taskMap.projectId, taskMap.allowedTaskIds),
-        );
-        setTasksAllowedForProjects(taskProjectMap);
-    };
-
-    const getLaborCodes = async () => {
-        if (apiEndpoint === undefined) {
-            throw Error('No REACT_APP_API_ENDPOINT has been set!');
-        }
-        const url = path.join(apiEndpoint, 'LaborCode');
-        const result = await fetch(url, { credentials: 'include' });
-
-        const laborCodesFromApi: DropdownOption[] = await result.json();
-        setLaborCodes(laborCodesFromApi);
-    };
-
-    const saveTimeSlip = async (
+    const saveTimeSlipAndStopAddingTimeSlip = async (
         projectId: number,
         taskId: number | null,
         laborCodeId: number | null,
         hours: number,
         minutes: number,
     ) => {
-        if (apiEndpoint === undefined) {
-            throw Error('No REACT_APP_API_ENDPOINT has been set!');
-        }
-        const url = path.join(apiEndpoint, 'TimeSlip/AddTimeSlip');
-        const result = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                projectId,
-                taskId,
-                laborCodeId,
-                hours,
-                minutes,
-                date,
-            }),
-        });
+        const response = await saveTimeSlip(projectId, taskId, laborCodeId, hours, minutes, date);
 
-        if (result.ok) {
+        if (response.ok) {
             setAddingTimeSlip(false);
         }
-    };
-
-    const getDateString = () => {
-        return date.toLocaleDateString('en');
-    };
-
-    useEffect(() => {
-        getProjects();
-        getTasks();
-        getTasksAllowedForProjects();
-        getLaborCodes();
-    }, []);
-
-    const getTaskOptionsForProject = (projectId: number): DropdownOption[] => {
-        const tasksForProject = tasksAllowedForProjects.get(projectId);
-        if (tasksForProject === undefined) {
-            throw Error('No tasks found for project!');
-        }
-
-        return tasksForProject.map((taskId: number) => {
-            const name = tasks.get(taskId);
-            if (name === undefined) {
-                throw Error('No tasks found for project!');
-            }
-
-            return { id: taskId, name };
-        });
     };
 
     const getAddTimeSlipButtonElements = (): JSX.Element => {
         if (addingTimeSlip) {
             return (
                 <EditableTimeSlip
-                    saveTimeSlip={saveTimeSlip}
-                    projectOptions={projects}
-                    laborCodeOptions={laborCodes}
+                    saveTimeSlip={saveTimeSlipAndStopAddingTimeSlip}
+                    projectOptions={projectOptions}
+                    laborCodeOptions={laborCodeOptions}
                     getTaskOptionsForProject={getTaskOptionsForProject}
                     stopAddingTimeslip={() => setAddingTimeSlip(false)}
                 />
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Home.module.scss';
 import { DayColumn } from './DayColumn';
 import { Day } from '../types/Day';
+import { DropdownOption } from '../types/DropdownOption';
+import path from 'path-browserify';
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+
+interface TaskMap {
+    projectId: number;
+    allowedTaskIds: number[];
+}
 
 const dayMap = new Map<number, Day>([
     [0, 'Sunday'],
@@ -14,10 +23,125 @@ const dayMap = new Map<number, Day>([
 ]);
 
 export const Home: React.FC<{}> = () => {
-    const date = new Date();
+    const currentDate = new Date();
+
+    const [projects, setProjects] = useState<DropdownOption[]>([]);
+    const [tasks, setTasks] = useState<Map<number, string>>(new Map<number, string>([]));
+
+    const [tasksAllowedForProjects, setTasksAllowedForProjects] = useState<Map<number, number[]>>(
+        new Map<number, number[]>([]),
+    );
+
+    const [laborCodes, setLaborCodes] = useState<DropdownOption[]>([]);
+
+    const getProjects = async () => {
+        if (apiEndpoint === undefined) {
+            throw Error('No REACT_APP_API_ENDPOINT has been set!');
+        }
+        const url = path.join(apiEndpoint, 'Project');
+        const result = await fetch(url, { credentials: 'include' });
+
+        const projectsFromApi: DropdownOption[] = await result.json();
+        setProjects(projectsFromApi);
+    };
+
+    const getTasks = async () => {
+        if (apiEndpoint === undefined) {
+            throw Error('No REACT_APP_API_ENDPOINT has been set!');
+        }
+        const url = path.join(apiEndpoint, 'Task');
+        const result = await fetch(url, { credentials: 'include' });
+
+        const tasksFromApi: DropdownOption[] = await result.json();
+
+        const taskMap = new Map<number, string>([]);
+        tasksFromApi.forEach((task) => taskMap.set(task.id, task.name));
+        setTasks(taskMap);
+    };
+
+    const getTasksAllowedForProjects = async () => {
+        if (apiEndpoint === undefined) {
+            throw Error('No REACT_APP_API_ENDPOINT has been set!');
+        }
+        const url = path.join(apiEndpoint, 'Task/ProjectsAllowedTasks');
+        const result = await fetch(url, { credentials: 'include' });
+
+        const taskMapsFromApi: TaskMap[] = await result.json();
+
+        const taskProjectMap = new Map<number, number[]>([]);
+        taskMapsFromApi.forEach((taskMap) =>
+            taskProjectMap.set(taskMap.projectId, taskMap.allowedTaskIds),
+        );
+        setTasksAllowedForProjects(taskProjectMap);
+    };
+
+    const getLaborCodes = async () => {
+        if (apiEndpoint === undefined) {
+            throw Error('No REACT_APP_API_ENDPOINT has been set!');
+        }
+        const url = path.join(apiEndpoint, 'LaborCode');
+        const result = await fetch(url, { credentials: 'include' });
+
+        const laborCodesFromApi: DropdownOption[] = await result.json();
+        setLaborCodes(laborCodesFromApi);
+    };
+
+    const saveTimeSlip = async (
+        projectId: number,
+        taskId: number | null,
+        laborCodeId: number | null,
+        hours: number,
+        minutes: number,
+        date: Date,
+    ): Promise<Response> => {
+        if (apiEndpoint === undefined) {
+            throw Error('No REACT_APP_API_ENDPOINT has been set!');
+        }
+        const url = path.join(apiEndpoint, 'TimeSlip/AddTimeSlip');
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                projectId,
+                taskId,
+                laborCodeId,
+                hours,
+                minutes,
+                date,
+            }),
+        });
+
+        return response;
+    };
+
+    useEffect(() => {
+        getProjects();
+        getTasks();
+        getTasksAllowedForProjects();
+        getLaborCodes();
+    }, []);
+
+    const getTaskOptionsForProject = (projectId: number): DropdownOption[] => {
+        const tasksForProject = tasksAllowedForProjects.get(projectId);
+        if (tasksForProject === undefined) {
+            throw Error('No tasks found for project!');
+        }
+
+        return tasksForProject.map((taskId: number) => {
+            const name = tasks.get(taskId);
+            if (name === undefined) {
+                throw Error('No tasks found for project!');
+            }
+
+            return { id: taskId, name };
+        });
+    };
 
     const getDayColumn = (day: 0 | 1 | 2 | 3 | 4 | 5 | 6) => {
-        const currentDay = date.getDay();
+        const currentDay = currentDate.getDay();
         const isCurrentDay = day === currentDay;
         const dayString = dayMap.get(day);
 
@@ -28,7 +152,17 @@ export const Home: React.FC<{}> = () => {
         const dayDate = new Date();
         dayDate.setDate(dayDate.getDate() + (day - currentDay));
 
-        return <DayColumn day={dayString} isCurrentDay={isCurrentDay} date={dayDate} />;
+        return (
+            <DayColumn
+                projectOptions={projects}
+                laborCodeOptions={laborCodes}
+                getTaskOptionsForProject={getTaskOptionsForProject}
+                saveTimeSlip={saveTimeSlip}
+                day={dayString}
+                isCurrentDay={isCurrentDay}
+                date={dayDate}
+            />
+        );
     };
 
     return (
